7029bb30a235e01c3da1a9db4087fa1d7cec7025
mmtv <mmtv.news.tw@gmail.com>
2023-05-31 19:39:45 +0800

    add GIPHY support

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,15 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-undef */
 /* global document, Office */
 
 const tenor_api_key = "AIzaSyAc2OphGysCfd2YVwWlIDd73yPzWJqGflM";
 const giphy_api_key = "Z81NqORdTMOb6Qhsm1PD4a2pzFiHOM0X";
 
+var limit_t = 10;
+var limit_g = 10;
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.PowerPoint) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("q").onchange = grab_data_from_tenor_luncher;
+    document.getElementById("q").onchange = grab_gif_luncher;
   }
 });
 
@@ -21,8 +23,44 @@ function insertImageLuncher(event) {
   }
 }
 
-function grab_data_from_tenor_luncher() {
-  grab_data_from_tenor(document.getElementById("q").value);
+window.addEventListener("scroll", function () {
+  if (isAtBottom()) {
+    limit_t += 10;
+    limit_g += 10;
+    grab_gif(document.getElementById("q").value, limit_g, limit_t);
+  }
+});
+
+function isAtBottom() {
+  var windowHeight = window.innerHeight;
+
+  var documentHeight = document.documentElement.scrollHeight;
+
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+  return scrollTop + windowHeight >= documentHeight;
+}
+
+function grab_gif_luncher() {
+  limit_t = limit_g = 10;
+  grab_gif(document.getElementById("q").value);
+  document.getElementById("loading-gifs").className = "";
+  document.getElementById("no-more-gifs").className = "no-display";
+}
+
+async function grab_gif(q = "money", lmt_g = limit_g, lmt_t = limit_t) {
+  var tenor_response_objects = [];
+  var giphy_response_objects = [];
+
+  try {
+    tenor_response_objects = await grab_data_from_tenor(q, lmt_t);
+  } catch (error) {
+    console.log(error);
+  }
+
+  giphy_response_objects = await grab_data_from_giphy(q, lmt_g);
+
+  insertGIFtoHtml(tenor_response_objects, giphy_response_objects);
 }
 
 function insertImage(url = "https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif") {
@@ -32,6 +70,46 @@ function insertImage(url = "https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/gip
       insertImageToPowerPoint(base64Image.split(",")[1]);
     })
     .catch((err) => console.log(err));
+}
+
+function insertGIFtoHtml(tenor, giphy) {
+  document.getElementById("tenor-gifs").innerHTML = "";
+
+  if (tenor.length < limit_g) {
+    document.getElementById("loading-gifs").className = "no-display";
+    document.getElementById("no-more-gifs").className = "";
+  }
+  for (var i = 0; i < tenor.length - 1; i++) {
+    var img_tag_t = document.createElement("img");
+    var container_t = document.getElementById("tenor-gifs");
+    container_t.appendChild(img_tag_t);
+    img_tag_t.src = tenor[i]["media_formats"]["nanogif"]["url"];
+    img_tag_t.alt = tenor[i]["media_formats"]["gif"]["url"];
+    img_tag_t.onclick = insertImageLuncher;
+    img_tag_t.className = "gif-preview";
+  }
+
+  document.getElementById("giphy-gifs").innerHTML = "";
+
+  if (giphy.length < limit_g) {
+    document.getElementById("loading-gifs").className = "no-display";
+    document.getElementById("no-more-gifs").className = "";
+  }
+  for (var j = 0; j < giphy.length - 1; j++) {
+    var img_tag_g = document.createElement("img");
+    var container_g = document.getElementById("giphy-gifs");
+
+    container_g.appendChild(img_tag_g);
+
+    img_tag_g.src = giphy[j]["images"]["fixed_height"]["url"];
+    console.log(giphy[j]["images"]["fixed_height"]["url"]); //debug
+    img_tag_g.alt = giphy[j]["images"]["original"]["url"];
+    console.log(giphy[j]["images"]["original"]["url"]); //debug
+    img_tag_g.onclick = insertImageLuncher;
+    img_tag_g.className = "gif-preview";
+  }
+
+  return;
 }
 
 function insertImageToPowerPoint(base64String) {
@@ -67,77 +145,51 @@ function convertImageToBase64FromURL(url) {
     });
 }
 
-// url Async requesting function
-function httpGetAsync(theUrl, callback) {
-  // create the request object
-  var xmlHttp = new XMLHttpRequest();
+function grab_data_from_tenor(search_term = "money", lmt = limit_t) {
+  return new Promise(function (resolve, reject) {
+    console.log("search_term: " + search_term); //debug
+    console.log("lmt: " + lmt); //debug
+    // set the apikey
+    var apikey = tenor_api_key;
+    var clientkey = "PowerGIF";
 
-  // set the state change callback to capture when the response comes in
-  xmlHttp.onreadystatechange = function () {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      callback(xmlHttp.responseText);
-    }
-  };
+    // using default locale of en_US
+    var search_url =
+      "https://tenor.googleapis.com/v2/search?q=" +
+      search_term +
+      "&key=" +
+      apikey +
+      "&client_key=" +
+      clientkey +
+      "&limit=" +
+      lmt;
 
-  // open as a GET call, pass in the url and set async = True
-  xmlHttp.open("GET", theUrl, true);
-
-  // call send with no params as they were passed in on the url string
-  xmlHttp.send(null);
-
-  return;
+    // send the http request
+    var http = new XMLHttpRequest();
+    http.open("GET", search_url, true);
+    http.onload = function () {
+      if (http.readyState === 4 && http.status === 200) {
+        var response = JSON.parse(http.responseText);
+        var tenor_response_objects = response["results"];
+        resolve(tenor_response_objects);
+      } else {
+        reject("Error: " + http.status);
+      }
+    };
+    http.send(null);
+  });
 }
 
-// callback for the top 8 GIFs of search
-function tenorCallback_search(responsetext) {
-  // Parse the JSON response
-  var response_objects = JSON.parse(responsetext);
-
-  var top_10_gifs = response_objects["results"];
-
-  // load the GIFs -- for our example we will load the first GIFs preview size (nanogif) and share size (gif)
-
-  document.getElementById("preview_gif_0").src = top_10_gifs[0]["media_formats"]["nanogif"]["url"];
-  document.getElementById("preview_gif_0").alt = top_10_gifs[0]["media_formats"]["gif"]["url"];
-  document.getElementById("preview_gif_0").onclick = insertImageLuncher;
-
-  for (var i = 1; i < top_10_gifs.length - 1; i++) {
-    var img_tag = document.createElement("img");
-    img_tag.id = "preview_gif_" + i;
-    var container = document.getElementById("tenor-gifs");
-    container.appendChild(img_tag);
-    document.getElementById("preview_gif_" + i).src = top_10_gifs[i]["media_formats"]["nanogif"]["url"];
-    document.getElementById("preview_gif_" + i).alt = top_10_gifs[i]["media_formats"]["gif"]["url"];
-    document.getElementById("preview_gif_" + i).onclick = insertImageLuncher;
-    document.getElementById("preview_gif_" + i).className = "gif-preview";
+function grab_data_from_giphy(q, lmt = limit_g) {
+  if (lmt >= 50) {
+    lmt = 49; //Giphy API limit
   }
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${giphy_api_key}&q=${q}&limit=${lmt}`;
 
-  return;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => console.log(error));
 }
 
-// function to call the trending and category endpoints
-function grab_data_from_tenor(search_term = "money") {
-  console.log("search_term: " + search_term);
-  // set the apikey and limit
-  var apikey = tenor_api_key;
-  var clientkey = "PowerGIF";
-  var lmt = 10;
-
-  // using default locale of en_US
-  var search_url =
-    "https://tenor.googleapis.com/v2/search?q=" +
-    search_term +
-    "&key=" +
-    apikey +
-    "&client_key=" +
-    clientkey +
-    "&limit=" +
-    lmt;
-
-  httpGetAsync(search_url, tenorCallback_search);
-
-  // data will be loaded by each call's callback
-  return;
-}
-
-grab_data_from_tenor();
+grab_gif();
